37020cafe924c1879149ff56041721fc354810ab
Jerry <jerry.ma.nk@gmail.com>
2023-06-14 12:19:22 -0400

    fix signal fitss viewer

--- src/tolteca_web/toltecSignalFitsViewer/__init__.py
+++ src/tolteca_web/toltecSignalFitsViewer/__init__.py
@@ -26,10 +26,10 @@ class ToltecSignalFitsViewer(ComponentTemplate):
         component_cls = dbc.Container
 
     def __init__(
-            self,
-            title_text="Toltec Signal Fits Viewer",
-            subtitle_text="(test version)",
-            **kwargs,
+        self,
+        title_text="Toltec Signal Fits Viewer",
+        subtitle_text="(test version)",
+        **kwargs,
     ):
         kwargs.setdefault("fluid", True)
         super().__init__(**kwargs)
@@ -37,14 +37,13 @@ class ToltecSignalFitsViewer(ComponentTemplate):
         self._subtitle_text = subtitle_text
         self.fluid = True
 
-
     def setup_layout(self, app):
         container = self
         header, body = container.grid(2, 1)
 
         # Again ... cheating
         self.tsf = None
-        
+
         # Header
         title_container = header.child(
             html.Div, className="d-flex align-items-baseline"
@@ -56,11 +55,12 @@ class ToltecSignalFitsViewer(ComponentTemplate):
             )
 
         # Hard code the input path for testing.
-        dPath = '/Users/wilson/Desktop/tmp/macs0717/pointing/102384/'
-        g = glob('{}redu*/'.format(dPath))
-        gp = glob('/Users/wilson/Desktop/tmp/cosmos/redu*/')
-        g = g+gp
-        pathOptions = [{'label': gg, 'value': gg} for gg in g]
+        dPath = "/Users/wilson/Desktop/tmp/macs0717/pointing/102384/"
+        g = glob("{}redu*/".format(dPath))
+        gp = glob("/Users/wilson/Desktop/tmp/cosmos/redu*/")
+        g = g + gp
+        g = g + list(glob("./redu*/coadded/raw/"))
+        pathOptions = [{"label": gg, "value": gg} for gg in g]
 
         # pull down to select signal fits path
         controls_panel, views_panel, bigBox = body.grid(3, 1)
@@ -70,46 +70,47 @@ class ToltecSignalFitsViewer(ComponentTemplate):
             dcc.Dropdown,
             options=pathOptions,
             placeholder="Select Path",
-            value='',
+            value="",
             searchable=False,
-            style=dict(
-                width='100%',
-                verticalAlign="middle"
-            ))
+            style=dict(width="100%", verticalAlign="middle"),
+        )
 
         # Put in a break
         bigBox.child(dbc.Row).child(html.Br)
 
         # Array, weight cut, edge trimming controls
         mainControlsTitlesRow = bigBox.child(dbc.Row)
-        arrayTitle = mainControlsTitlesRow.child(dbc.Col, width=3).child(html.H5,
-                                                                         "Array Choice",
-                                                                         className='mb-2')
-        wtcutTitle = mainControlsTitlesRow.child(dbc.Col, width=3).child(html.H5,
-                                                                         "Weight Cut",
-                                                                         className='mb-2')
-        tzTitle = mainControlsTitlesRow.child(dbc.Col, width=2).child(html.H5,
-                                                                      "Trim Map Zeros",
-                                                                      className='mb-2')
+        arrayTitle = mainControlsTitlesRow.child(dbc.Col, width=3).child(
+            html.H5, "Array Choice", className="mb-2"
+        )
+        wtcutTitle = mainControlsTitlesRow.child(dbc.Col, width=3).child(
+            html.H5, "Weight Cut", className="mb-2"
+        )
+        tzTitle = mainControlsTitlesRow.child(dbc.Col, width=2).child(
+            html.H5, "Trim Map Zeros", className="mb-2"
+        )
         mainControlsRow = bigBox.child(dbc.Row)
         arrayCol = mainControlsRow.child(dbc.Col, width=3)
         arrayChoice = arrayCol.child(
             dcc.RadioItems,
-            options=[{'label': 'a1100', 'value': 'a1100', 'disabled': True},
-                     {'label': 'a1400', 'value': 'a1400', 'disabled': True},
-                     {'label': 'a2000', 'value': 'a2000', 'disabled': True},],
+            options=[
+                {"label": "a1100", "value": "a1100", "disabled": True},
+                {"label": "a1400", "value": "a1400", "disabled": True},
+                {"label": "a2000", "value": "a2000", "disabled": True},
+            ],
             inline=True,
-            inputStyle={"margin-right": "5px", "margin-left": "20px"})
+            inputStyle={"marginRight": "5px", "marginLeft": "20px"},
+        )
         weightCol = mainControlsRow.child(dbc.Col, width=3)
         weightCut = getWeightCutControl(weightCol)
         trimEdgeCol = mainControlsRow.child(dbc.Col, width=1)
-        trimEdgeSel = trimEdgeCol.child(
-            daq.ToggleSwitch,
-            default=True)
-        mainControls = {'array': arrayChoice,
-                        'weightCut': weightCut,
-                        'trimEdgeSelector': trimEdgeSel}
-        
+        trimEdgeSel = trimEdgeCol.child(daq.ToggleSwitch, default=True)
+        mainControls = {
+            "array": arrayChoice,
+            "weightCut": weightCut,
+            "trimEdgeSelector": trimEdgeSel,
+        }
+
         # Put in a break
         bigBox.child(dbc.Row).child(html.Br)
 
@@ -123,12 +124,14 @@ class ToltecSignalFitsViewer(ComponentTemplate):
             "weight": weight_plot,
             "s2n": s2n_plot,
         }
-        
+
         # Another break
         bigBox.child(dbc.Row).child(html.Br)
-        
+
         # Map Histogram
-        imageOptions = [{'label': a, 'value': a} for a in ['signal', 'weight', 'sig2noise']]
+        imageOptions = [
+            {"label": a, "value": a} for a in ["signal", "weight", "sig2noise"]
+        ]
         histBox = bigBox.child(dbc.Row)
         histPlotPanel = histBox.child(dbc.Row)
         histControlPanel = histBox.child(dbc.Row)
@@ -136,20 +139,20 @@ class ToltecSignalFitsViewer(ComponentTemplate):
             dcc.RadioItems,
             options=imageOptions,
             inline=True,
-            value='signal',
+            value="signal",
         )
-        histPlot = histPlotPanel.child(dcc.Loading, type='circle').child(dcc.Graph)
-        pixelHistogram = {'controls': histControl,
-                          'plot': histPlot}
+        histPlot = histPlotPanel.child(dcc.Loading, type="circle").child(dcc.Graph)
+        pixelHistogram = {"controls": histControl, "plot": histPlot}
 
-
-        self._registerCallbacks(app, fitsPath_select, pixelHistogram, images,
-                                mainControls)
+        super().setup_layout(app)
+        self._registerCallbacks(
+            app, fitsPath_select, pixelHistogram, images, mainControls
+        )
         return
 
-
-    def _registerCallbacks(self, app, fitsPath_select, pixelHistogram, images,
-                           mainControls):
+    def _registerCallbacks(
+        self, app, fitsPath_select, pixelHistogram, images, mainControls
+    ):
         print("Registering Callbacks")
 
         # ---------------------------
@@ -157,41 +160,39 @@ class ToltecSignalFitsViewer(ComponentTemplate):
         # ---------------------------
         @app.callback(
             [
-                Output(pixelHistogram['plot'].id, "figure"),
-                Output(mainControls['array'].id, 'options'),
-                Output(mainControls['array'].id, 'value'),
+                Output(pixelHistogram["plot"].id, "figure"),
+                Output(mainControls["array"].id, "options"),
+                Output(mainControls["array"].id, "value"),
             ],
             [
-                Input(fitsPath_select.id, 'value'),
-                Input(pixelHistogram['controls'].id, 'value'),
-                Input(mainControls['weightCut'].id, 'value'),
-                Input(mainControls['array'].id, 'options'),
-                Input(mainControls['array'].id, 'value'),
+                Input(fitsPath_select.id, "value"),
+                Input(pixelHistogram["controls"].id, "value"),
+                Input(mainControls["weightCut"].id, "value"),
+                Input(mainControls["array"].id, "options"),
+                Input(mainControls["array"].id, "value"),
             ],
-            prevent_initial_call=True
+            prevent_initial_call=True,
         )
         def primaryDropdown(path, histImage, weightCut, arrayOpt, array):
-            if((path == '') | (path is None)):
+            if (path == "") | (path is None):
                 raise PreventUpdate
 
             # check which array files exist at this path
             arrayOptions = getArrayOptions(path, arrayOpt)
             availableArrays = []
             for a in arrayOptions:
-                if a['disabled'] == False:
-                    availableArrays.append(a['value'])
-            if(len(availableArrays) == 0):
-                raise ValueError(
-                    "No FITS files available for use at this path.")
-            if(array is None):
+                if a["disabled"] == False:
+                    availableArrays.append(a["value"])
+            if len(availableArrays) == 0:
+                raise ValueError("No FITS files available for use at this path.")
+            if array is None:
                 array = availableArrays[0]
 
             tsf = ToltecSignalFits(path=path, array=array)
             tsf.setWeightCut(weightCut)
-            histImage = histImage + '_I'
+            histImage = histImage + "_I"
             histFig = tsf.plotMapHistogram(histImage, returnPlotlyPlot=True)
             return [histFig, arrayOptions, array]
-
 
         # ---------------------------
         # Big three panel fig
@@ -203,10 +204,10 @@ class ToltecSignalFitsViewer(ComponentTemplate):
                 images["s2n"].component_output,
             ],
             [
-                Input(fitsPath_select.id, 'value'),
-                Input(mainControls['weightCut'].id, 'value'),
-                Input(mainControls['array'].id, 'value'),
-                Input(mainControls['trimEdgeSelector'].id, 'value'),
+                Input(fitsPath_select.id, "value"),
+                Input(mainControls["weightCut"].id, "value"),
+                Input(mainControls["array"].id, "value"),
+                Input(mainControls["trimEdgeSelector"].id, "value"),
                 images["signal"].component_inputs,
                 images["weight"].component_inputs,
                 images["s2n"].component_inputs,
@@ -214,26 +215,28 @@ class ToltecSignalFitsViewer(ComponentTemplate):
             prevent_initial_call=True,
         )
         def threePanelFig(path, weightCut, array, trimEdges, *sp_inputs_list):
-            if((path is None) | (path == '')):
+            if (path is None) | (path == ""):
                 raise PreventUpdate
             outputs = []
-            for sp_inputs, name, signal in zip(sp_inputs_list,
-                                               ["signal", "weight", "s2n"],
-                                               ["signal_I", "weight_I", "sig2noise_I"]):
-                image_data, _ = getImage(signal, path, weightCut, array, trimEdge=trimEdges)
+            for sp_inputs, name, signal in zip(
+                sp_inputs_list,
+                ["signal", "weight", "s2n"],
+                ["signal_I", "weight_I", "sig2noise_I"],
+            ):
+                image_data, _ = getImage(
+                    signal, path, weightCut, array, trimEdge=trimEdges
+                )
                 plotTitle = "{0:}".format(signal)
                 outputs.append(
                     images[name].make_figure_data(
                         image_data,
                         title=plotTitle,
-                        aspect="auto",
+                        # aspect="auto",
                         **sp_inputs,
                     )
                 )
             return outputs
 
-
-        
         # # ---------------------------
         # # Signal Fancy Fig
         # # ---------------------------
@@ -256,7 +259,6 @@ class ToltecSignalFitsViewer(ComponentTemplate):
         #         raise PreventUpdate
         #     hfig, imfig = getFancyFig('signal_I', path, hrange, weightCut, array, trimEdge)
         #     return [hfig, imfig]
-
 
         # # ---------------------------
         # # Weight Fancy Fig
@@ -283,7 +285,6 @@ class ToltecSignalFitsViewer(ComponentTemplate):
         #     hfig, imfig = getFancyFig('weight_I', path, hrange, weightCut, array, trimEdge)
         #     return [hfig, imfig]
 
-
         # # ---------------------------
         # # S/N Fancy Fig
         # # ---------------------------
@@ -309,7 +310,6 @@ class ToltecSignalFitsViewer(ComponentTemplate):
         #     hfig, imfig = getFancyFig('sig2noise_I', path, hrange, weightCut, array, trimEdge)
         #     return [hfig, imfig]
 
-        
 
 # Returns the control for the weight cut
 def getWeightCutControl(box):
@@ -318,7 +318,7 @@ def getWeightCutControl(box):
         min=0.0,
         max=1.0,
         step=0.1,
-        value = 0.2,
+        value=0.2,
         tickformat=".2f",
     )
     return weightRange
@@ -355,13 +355,13 @@ def getImage(name, path, weightCut, array, trimEdge=False):
     image = tsf.getMap(name)
     wcs = tsf.getMapWCS(name)
 
-    if(trimEdge):
+    if trimEdge:
         # generate a cutout to trim out the zeros on the border
         nz = np.nonzero(image)
-        xsize = nz[0].max()-nz[0].min()
-        ysize = nz[1].max()-nz[1].min()
-        xpos = int((nz[0].min() + nz[0].max())/2.)
-        ypos = int((nz[1].min() + nz[1].max())/2.)
+        xsize = nz[0].max() - nz[0].min()
+        ysize = nz[1].max() - nz[1].min()
+        xpos = int((nz[0].min() + nz[0].max()) / 2.0)
+        ypos = int((nz[1].min() + nz[1].max()) / 2.0)
         signal = Cutout2D(image, (ypos, xpos), (ysize, xsize), wcs=wcs)
         image = signal.data
         wcs = signal.wcs
@@ -369,13 +369,12 @@ def getImage(name, path, weightCut, array, trimEdge=False):
 
 
 def getFancyFig(name, path, ranges, weightCut, array, trimEdge):
-
     # fetch the image and coordinates
     image, wcs = getImage(name, path, weightCut, array, trimEdge=trimEdge)
     s = image.shape
     x = wcs.pixel_to_world(np.arange(0, s[0]), 0)
     y = wcs.pixel_to_world(0, np.arange(0, s[1]))
-    if(wcs.wcs.ctype[0] == 'AZOFFSET'):
+    if wcs.wcs.ctype[0] == "AZOFFSET":
         x = x[0]
         y = y[1]
         if max(image.shape) < 1000:
@@ -389,77 +388,86 @@ def getFancyFig(name, path, ranges, weightCut, array, trimEdge):
             ulabel = "[deg.]"
         x = x.to(units).value
         y = y.to(units).value
-    elif(wcs.wcs.ctype[0] == 'RA---TAN'):
+    elif wcs.wcs.ctype[0] == "RA---TAN":
         ulabel = "[deg.]"
         x = x.ra.value
         y = y.dec.value
-        
+
     # Flatten and get rid of the zeros
     signal = image.flatten()
-    signal = signal[np.where(signal != 0.)[0]]
+    signal = signal[np.where(signal != 0.0)[0]]
 
     # Deal with ranges
-    if(ranges[0] == -99):
+    if ranges[0] == -99:
         ranges[0] = signal.min()
-    if(ranges[1] == 99):
+    if ranges[1] == 99:
         ranges[1] = signal.max()
 
     # Generate the histogram figure
     h, bins = np.histogram(signal, bins=50, range=ranges)
-    bincenters = np.array([(bins[i]+bins[i+1])/2. for i in range(len(bins)-1)])
+    bincenters = np.array([(bins[i] + bins[i + 1]) / 2.0 for i in range(len(bins) - 1)])
     hfig = go.Figure()
-    hfig.add_trace(go.Bar(x=bincenters, y=h, name=''))
-    hfig.update_layout(plot_bgcolor='white')
+    hfig.add_trace(go.Bar(x=bincenters, y=h, name=""))
+    hfig.update_layout(plot_bgcolor="white")
     hfig.update_yaxes(visible=False, showticklabels=False)
-    margin=go.layout.Margin(
-        l=10, #left margin
-        r=10, #right margin
-        b=5, #bottom margin
-        t=40, #top margin
+    margin = go.layout.Margin(
+        l=10,  # left margin
+        r=10,  # right margin
+        b=5,  # bottom margin
+        t=40,  # top margin
     )
     plotTitle = name
-    hfig.update_layout(margin=margin, height=60,
-                       title={'text': plotTitle,
-                              'x': 0.5,},
-                       )
+    hfig.update_layout(
+        margin=margin,
+        height=60,
+        title={
+            "text": plotTitle,
+            "x": 0.5,
+        },
+    )
 
-    
     # Generate the image figure
-    if((image.shape[0]>2000) | (image.shape[1]>2000)):
-        bs=True
+    if (image.shape[0] > 2000) | (image.shape[1] > 2000):
+        bs = True
     else:
-        bs=False
+        bs = False
 
-    if(bs):
-        imfig = px.imshow(image, x=x, y=y, zmin=ranges[0], zmax=ranges[1],
-                          binary_string=bs, origin='lower')
-    else:
-        imfig = go.Figure()
-        imfig.add_trace(go.Heatmap(
-            z=image,
+    if bs:
+        imfig = px.imshow(
+            image,
             x=x,
             y=y,
             zmin=ranges[0],
             zmax=ranges[1],
-        ))
+            binary_string=bs,
+            origin="lower",
+        )
+    else:
+        imfig = go.Figure()
+        imfig.add_trace(
+            go.Heatmap(
+                z=image,
+                x=x,
+                y=y,
+                zmin=ranges[0],
+                zmax=ranges[1],
+            )
+        )
 
     imfig.update_layout(
-        uirevision=True,
-        showlegend=False,
-        autosize=True,
-        plot_bgcolor='white'
+        uirevision=True, showlegend=False, autosize=True, plot_bgcolor="white"
     )
-    imfig.update_xaxes(range=[x.min(), x.max()],
-                       title=wcs.wcs.ctype[0]+" "+ulabel)
-    imfig.update_yaxes(range=[y.min(), y.max()], scaleanchor = 'x',
-                       title=wcs.wcs.ctype[1]+" "+ulabel)
+    imfig.update_xaxes(range=[x.min(), x.max()], title=wcs.wcs.ctype[0] + " " + ulabel)
+    imfig.update_yaxes(
+        range=[y.min(), y.max()], scaleanchor="x", title=wcs.wcs.ctype[1] + " " + ulabel
+    )
 
     # px.imshow(image, x=x, y=y, zmin=ranges[0], zmax=ranges[1], binary_string=bs, origin='lower')
-    margin=go.layout.Margin(
-        l=10, #left margin
-        r=10, #right margin
-        b=20, #bottom margin
-        t=0, #top margin
+    margin = go.layout.Margin(
+        l=10,  # left margin
+        r=10,  # right margin
+        b=20,  # bottom margin
+        t=0,  # top margin
     )
     imfig.update_layout(margin=margin, height=400)
     return [hfig, imfig]
@@ -472,42 +480,42 @@ def getXYAxisLayouts():
         showline=True,
         showgrid=False,
         showticklabels=True,
-        linecolor='black',
+        linecolor="black",
         linewidth=4,
-        ticks='outside',
+        ticks="outside",
         tickfont=dict(
-            family='Arial',
+            family="Arial",
             size=18,
-            color='rgb(82, 82, 82)',
+            color="rgb(82, 82, 82)",
         ),
     )
 
     yaxis = dict(
         titlefont=dict(size=20),
-        scaleanchor = 'x',
+        scaleanchor="x",
         showline=True,
         showgrid=False,
         showticklabels=True,
-        linecolor='black',
+        linecolor="black",
         linewidth=4,
-        ticks='outside',
+        ticks="outside",
         tickfont=dict(
-            family='Arial',
+            family="Arial",
             size=18,
-            color='rgb(82, 82, 82)',
+            color="rgb(82, 82, 82)",
         ),
     )
     return xaxis, yaxis
 
 
 def getArrayOptions(path, options):
-    g = glob(path+"toltec*.fits")
-    for j, a in enumerate(['a1100', 'a1400', 'a2000']):
+    g = glob(path + "toltec*.fits")
+    for j, a in enumerate(["a1100", "a1400", "a2000"]):
         f = [i for i in g if a in i]
-        if(len(f) > 0):
-            options[j]['disabled'] = False
+        if len(f) > 0:
+            options[j]["disabled"] = False
         else:
-            options[j]['disabled'] = True
+            options[j]["disabled"] = True
     return options
 
 
